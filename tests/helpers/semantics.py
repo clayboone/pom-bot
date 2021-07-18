9474e50339a3d2b90c25635b2f806fd3c4e1170f
Clay Boone <tener.hades@gmail.com>
2021-07-18 18:11:23 +0100

    refactor semantics

--- tests/helpers/semantics.py
+++ tests/helpers/semantics.py
@@ -1,20 +1,22 @@
+import unittest
 from contextlib import contextmanager
 
 
 @contextmanager
-def assert_not_raises(*, test: object = None, msg: str = None):
-    """Context manager that does nothing.
+def assert_not_raises(testcase: unittest.TestCase = None, msg: str = None):
+    """Context manager that does nothing; Semantic sugar for the reader to know
+    that purpose of a line is that it does not raise an exception.
 
-    When an exception occurs, it will be allowed raise.  This is semantic
-    sugar for the reader to know that the purpose of a line is to ensure that
-    the line does not raise an exception.
+    Takes two optional, mutually inclusive arguments. When an exception occurs,
+    then `testcase.fail` will be called by passing `msg` as the argument.
 
-    Takes an optional keyword-only `msg` argument. When an exception is raise,
-    print msg to stderr.
+    @param testcase The object of the specific test being run.
+    @param msg A custom message to send to the output on failure.
     """
     try:
         yield
-    except Exception:
-        if test and msg:
-            test.fail(msg)
-        raise
+    except Exception as exc:
+        if testcase and msg:
+            testcase.fail(msg)
+
+        raise AssertionError("Exception was not expected") from exc
